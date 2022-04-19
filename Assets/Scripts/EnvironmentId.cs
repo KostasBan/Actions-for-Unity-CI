8f5a01d8a36a59a3160208aff8c9ad21387529da
Kostas Banakakis <mpanakak@hotmail.com>
2022-04-19 23:58:47 +0300

    environment variable

---
+++ Assets/Scripts/EnvironmentId.cs
@@ -0,0 +1,5 @@
+[System.Serializable]
+public class EnvironmentId
+{
+    public string id;
+}
--- Assets/Scripts/RemoteConfigManager.cs
+++ Assets/Scripts/RemoteConfigManager.cs
@@ -2,20 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
 using Unity.RemoteConfig;
+using System.Threading.Tasks;
 
 public class RemoteConfigManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Text _environmentVisualizer = null;
+    public TextAsset _textAsset = null;
+    public struct userAttributes
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public struct appAttributes
     {
-        
+        public string appVersion;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Add a listener to apply settings when successfully retrieved:
+        ConfigManager.FetchCompleted += ApplyRemoteSettings;
+
+        EnvironmentId environment = (EnvironmentId)JsonUtility.FromJson<EnvironmentId>(_textAsset.text);
+
+        // Set the environment ID:
+        ConfigManager.SetEnvironmentID(environment.id);
+
+        // Fetch configuration settings from the remote service:
+        ConfigManager.FetchConfigs<userAttributes, appAttributes>(new userAttributes(), new appAttributes());
+    }
+
+    void ApplyRemoteSettings(ConfigResponse configResponse)
+    {
+        switch (configResponse.requestOrigin)
+        {
+            case ConfigOrigin.Default:
+                Debug.Log("No settings loaded this session; using default values.");
+                break;
+            case ConfigOrigin.Cached:
+                Debug.Log("No settings loaded this session; using cached values from a previous session.");
+                break;
+            case ConfigOrigin.Remote:
+                Debug.Log("New settings loaded this session; update values accordingly.");
+                _environmentVisualizer.text = ConfigManager.appConfig.GetString("Environment");
+                break;
+        }
     }
 }
+
